b3ab79673ff5207066d1274458e3a6cf7bb08d01
salomeV <mvasquezo10@miumg.edu.gt>
2021-09-18 23:21:06 -0600

    Se agregraon las funciones: borrar todo, borrar uno a uno y toda la seccion de calculadora basica

--- operaciones.js
+++ operaciones.js
@@ -1,1 +1,33 @@
+var input_content = document.getElementById("calculo").value;
 
+//Declaring functions to both sections (scientific calculator and basic calculos)
+function todoEliminado (tecla) {    
+    switch(tecla) {
+    
+                    case 'C':
+                        document.getElementById("calculo").value = ' ';
+                    break;
+    
+                    case '=':
+                    var resultado = eval(document.getElementById("calculo").value);
+                    document.getElementById("calculo").value = resultado;
+                    break;
+    
+                    default:
+                        document.getElementById("calculo").value = document.getElementById("calculo").value + tecla;
+                    break;
+    }
+}
+
+function unEliminado(){
+    var actual = document.getElementById("calculo").value;
+    var nuevoIngreso = actual.substring(0, actual.length-1);
+
+    document.getElementById("calculo").value = nuevoIngreso;
+}
+
+function masMenos(){
+    var actual = document.getElementById("calculo").value;
+    var nuevoIngreso = actual * -1;
+    document.getElementById("calculo").value = nuevoIngreso;
+}
